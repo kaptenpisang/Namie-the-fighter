b91ff2a4c6860706f1737aecbfd0c73e12c9b39f
kaptenpisang <kaptenpisangbreakstation@gmail.com>
2025-09-08 18:50:56 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>🔥 Namie The Fighter 🔥</h1>
+      <p>Welcome to Arc 1 - Daily Quests Begin!</p>
+    </div>
+  );
+}
+
+export default App;
